3ee0e6d516b57400d8baa3cd66b0134381ac2675
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-04-26 17:23:45 +0300

    Added memoization for fetch user function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -9,11 +10,15 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   });
-};
+});
