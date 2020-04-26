a7ba59d3022ecca156ce79b83b6cc3d2af57d26f
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-04-26 17:57:42 +0300

    Added lodash chain instead of different methods

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId')); // get state posts, get all user ids from and take only unique
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+}
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
@@ -10,15 +23,11 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   });
-});
+};
